56e7298b19789cf3d5746f266682604b1b7b5232
ProgramX-NPledger <nathan@programx.co.uk>
2025-07-17 16:26:40 +0100

    End 3/6

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'bot-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Initialization logic can go here
+  }
+
+  // Additional methods can be added here if needed
 
 }
