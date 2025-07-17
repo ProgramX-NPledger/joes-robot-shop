4a0626530bdf00df6758e114a6ffb25ec5ede570
ProgramX-NPledger <nathan@programx.co.uk>
2025-07-17 16:45:27 +0100

    End 4/3

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.model';
 
 @Component({
   selector: 'bot-catalog',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
+  product: IProduct;
 
+  constructor() {
+    this.product = {
+      id: 1,
+      description: 'A high-quality robot vacuum cleaner',
+      name: 'Robot Vacuum',
+      category: 'Home Appliances',
+      price: 299.99,
+      imageName: 'robot.png',
+      discount: 10
+    };
+  }
 }
---
+++ src/app/catalog/product.model.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    id: number;
+    description: string;
+    name: string;
+    category: string;
+    price: number;
+    imageName: string;
+    discount: number;
+}
