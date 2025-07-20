250c15a1f6dfb841ce0e48a96be484f4f981953c
ProgramX-NPledger <nathan@programx.co.uk>
2025-07-20 11:30:15 +0100

    End 8/5

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './catalog/product.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   cart: IProduct[] = [];
-  
-  add(product: IProduct) {
-    this.cart.push(product);
-    console.log(`Added ${product.name} to cart.`);
-  }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  add(product: IProduct) {
+    this.http.post('/api/cart', this.cart).subscribe({
+      next: () => console.log(`Added ${product.name} to cart.`),
+      error: (err) => console.error(`Error adding ${product.name} to cart:`, err)
+    });
+    this.cart.push(product);
+    //console.log(`Added ${product.name} to cart.`);
+  }
 }
